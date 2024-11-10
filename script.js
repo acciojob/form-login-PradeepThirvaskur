29b9c900115e8c127483a853386428580ca94774
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-10 19:08:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
-    //Write your code here
 
-}
+   function getFormvalue(){
+    let input= document.qyerySelectorAll("input[type]='text'");
+	   let firstName=input[0];
+	   let lastName=input[1];
+	   alert(`${firstName} ${lastName}`);
+    }
+
