23edbb0e391d1c84d7f1d75889ae70cae90c0b8f
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-10 19:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-   function getFormvalue(){
+   function getFormvalue(event){
+	   event.preventDefault(); 
+	   // This line prevents the form from 
+	   //being submitted in the default manner (i.e., no page refresh).
     let input= document.qyerySelectorAll("input[type]='text'");
 	   let firstName=input[0];
 	   let lastName=input[1];
